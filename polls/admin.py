2a4a27ad109d0755e7c63fb78106d533442af934
Shy_Same <olyaluchko@gmail.com>
2021-08-16 16:14:40 +0300

    command_User_Add

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Choice, Question
+from .models import Choice, Question,PollsUser
 
 
 class ChoiceInline(admin.TabularInline):
@@ -20,3 +20,5 @@ class QuestionAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(PollsUser)
---
+++ polls/management/commands/user_add.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+from polls.models import PollsUser as Pu
+
+import ctypes
+
+from faker import Faker
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('total', type=int, help=u'Количество создаваемых пользователей')
+
+    def handle(self, *args, **kwargs):
+        number = kwargs['total']
+        if 0 < number < 11:
+            fake = Faker()
+            list_pu = []
+            for i in range(number):
+                list_pu.append(Pu(username=fake.name(), email=fake.ascii_email(), password=fake.swift()))
+            Pu.objects.bulk_create(list_pu)
+            self.stdout.write('Successfully created')
+        else:
+            self.stdout.write("The number must be in the range 1 to 10")
+
