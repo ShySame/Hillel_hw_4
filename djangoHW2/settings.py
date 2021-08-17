3da4b19226260f5bb77cae25363a5c513ed150df
Shy_Same <olyaluchko@gmail.com>
2021-08-17 12:44:24 +0300

    secret_key

--- djangoHW2/settings.py
+++ djangoHW2/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&2reks$2va1hkwximnr&r1np-klrs5i_4#)%-^$k1!2c!48_ud'
+SECRET_KEY = os.environ.get("SECRET_KEY",
+                            'django-insecure-&2reks$2va1hkwximnr&r1np-klrs5i_4#)%-^$k1!2c!48_ud')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
