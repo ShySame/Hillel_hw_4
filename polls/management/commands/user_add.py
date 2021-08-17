d4508536db8f46241c8c71abac02517ee572126d
Shy_Same <olyaluchko@gmail.com>
2021-08-17 12:40:12 +0300

    hash

--- polls/management/commands/user_add.py
+++ polls/management/commands/user_add.py
@@ -1,14 +1,14 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
+from django.core.management.base import BaseCommand, CommandError
 
 from faker import Faker
-
-from django.contrib.auth import get_user_model
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('total', type=int, choices=range(1, 11),  help=u'Количество создаваемых пользователей')
+        parser.add_argument('total', type=int, choices=range(1, 11),
+                            help=u'Количество создаваемых пользователей')
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
@@ -18,7 +18,7 @@ class Command(BaseCommand):
             list_pu = []
             for i in range(number):
                 list_pu.append(User(username=fake.name(), email=fake.ascii_email(),
-                                  password=make_password(fake.swift())))
+                                    password=make_password(fake.swift())))
             User.objects.bulk_create(list_pu)
             self.stdout.write('Successfully created')
         except User.DoesNotExist:
