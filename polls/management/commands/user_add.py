f9db53e31e573b7344db5d798122adf4a38315aa
Shy_Same <olyaluchko@gmail.com>
2021-08-17 12:28:42 +0300

    choices arg add

--- polls/management/commands/user_add.py
+++ polls/management/commands/user_add.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 from faker import Faker
 
@@ -7,11 +7,11 @@ from polls.models import PollsUser as Pu
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('total', type=int, help=u'Количество создаваемых пользователей')
+        parser.add_argument('total', type=int, choices=range(1, 11),  help=u'Количество создаваемых пользователей')
 
     def handle(self, *args, **kwargs):
         number = kwargs['total']
-        if 0 < number < 11:
+        try:
             fake = Faker()
             list_pu = []
             for i in range(number):
@@ -19,5 +19,5 @@ class Command(BaseCommand):
                                   password=fake.swift()))
             Pu.objects.bulk_create(list_pu)
             self.stdout.write('Successfully created')
-        else:
-            self.stdout.write("The number must be in the range 1 to 10")
+        except Pu.DoesNotExist:
+            raise CommandError("The number must be in the range 1 to 10")
