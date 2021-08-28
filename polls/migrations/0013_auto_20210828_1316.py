5da50193cd6c0a28d0ce7ec1467c6dd01e593130
Shy_Same <olyalychko@gmail.com>
2021-08-28 16:32:18 +0300

    dz9

--- polls/migrations/0013_auto_20210828_1316.py
+++ polls/migrations/0013_auto_20210828_1316.py
@@ -17,7 +17,8 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='person',
             name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False,
+                                      verbose_name='ID'),
             preserve_default=False,
         ),
     ]
